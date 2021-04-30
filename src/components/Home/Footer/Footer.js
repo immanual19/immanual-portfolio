cd788f411fec42afb055bd8172a5ba0620a3709f
immanual19 <immanuelsarker1@gmail.com>
2021-05-01 02:08:28 +0600

    my-portfolio

--- src/components/Home/Footer/Footer.js
+++ src/components/Home/Footer/Footer.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub,faLinkedin,faFacebook } from '@fortawesome/free-brands-svg-icons';
 import './Footer.css';
 const Footer = () => {
+
+    const handleGithubClick=()=>{
+        console.log('')
+    }
     return (
-        
-        <div className="my-5 mx-5">
+        <div className="mt-5 mx-5">
         <div className="footer-section">
-            <h1>This will be the Footer</h1>
+            <a href="https://github.com/immanual19" target="_blank"><FontAwesomeIcon className="footer-icon" icon={faGithub} /></a>
+            <a href="https://www.linkedin.com/in/immanual1/"><FontAwesomeIcon className="footer-icon" icon={faLinkedin} /></a>
+            <a href="https://www.facebook.com/immanual1"><FontAwesomeIcon className="footer-icon" icon={faFacebook} /></a>
+            <br/><br/>
+            <p>Designed at Developed by A. Immanual Sarker</p>
+            <p><small>Copyright &#169; {(new Date()).getFullYear()} A. Immanual Sarker</small></p>
         </div>   
         </div>
     );
