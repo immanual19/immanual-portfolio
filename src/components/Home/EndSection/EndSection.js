f354a658a0db0d1f0628e2ded92cfd633907a3ea
immanual19 <immanuelsarker1@gmail.com>
2021-05-01 05:52:50 +0600

    bugFixed

--- src/components/Home/EndSection/EndSection.js
+++ src/components/Home/EndSection/EndSection.js
@@ -11,7 +11,7 @@ const EndSection = () => {
             <p><small>I am always interested in hearing from you. You can knock me to say 'Hello' even if you do not have any plan to colaborate with me. I am open to any kid of suggestions, constructive criticism or casual discussion about web development. So, why delay? Hit the 'Contact Me' button and I will get back to you as soon as possible.</small></p>
             <br/><br/>
             <button className="downloadHireButton"> <a href="https://drive.google.com/uc?id=1aNyoCmmQTW0Hx3MSvCSpxYex7Ug0Hnot&export=download">Download Resume</a></button>
-            <a href="http://localhost:3000/contact"><button className="downloadHireButton"> Contact Me</button></a>
+            <a href="https://immanual-portfolio.web.app/contact"><button className="downloadHireButton"> Contact Me</button></a>
         </motion.div>
     );
 };
