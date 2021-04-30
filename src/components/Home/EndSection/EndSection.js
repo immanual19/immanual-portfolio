0d74bd02d728643a387955ae792591f5ef496a23
immanual19 <immanuelsarker1@gmail.com>
2021-05-01 02:31:05 +0600

    my-portfolio

--- src/components/Home/EndSection/EndSection.js
+++ src/components/Home/EndSection/EndSection.js
@@ -6,7 +6,7 @@ const EndSection = () => {
         <motion.div
         whileHover={{ scale: 1.09 }}
         className="end-section">
-            <h4 className="ending-header">INTERESTED IN COLABORAING?</h4>
+            <h4 className="ending-header">INTERESTED IN COLABORATING?</h4>
             <p>Hit the 'Contact Me' button below</p>
             <p><small>I am always interested in hearing from you. You can knock me to say 'Hello' even if you do not have any plan to colaborate with me. I am open to any kid of suggestions, constructive criticism or casual discussion about web development. So, why delay? Hit the 'Contact Me' button and I will get back to you as soon as possible.</small></p>
             <br/><br/>
